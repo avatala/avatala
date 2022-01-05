e03535ed68a3ea387607d2d36423aa64bb43fa7a
avatala <avatalavpr@gmail.com>
2022-01-05 17:07:11 +0000

    startup file is added

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ resource "google_compute_instance" "my-vm" {
   machine_type            = "f1-micro"
   tags                    = ["web", "http-server"]
   zone                    = "us-central1-a"
-  metadata_startup_script = 
+  metadata_startup_script = file("startup.sh")
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
